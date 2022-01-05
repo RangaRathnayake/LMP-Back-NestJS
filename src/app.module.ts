e6f940f3d4baa295eedbe5047b172f231c8da393
Ranga Rathnayake <58025781+RangaRathnayake@users.noreply.github.com>
2022-01-05 20:37:24 +0530

    customer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { CustomerModule } from './customer/customer.module';
 
 
 @Module({
@@ -22,6 +23,7 @@ import { UserModule } from './user/user.module';
       synchronize: true
     }),
     UserModule,
+    CustomerModule,
 
   ],
   controllers: [AppController],
