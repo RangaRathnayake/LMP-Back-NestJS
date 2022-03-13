3ca453159581e8cfadcaa2208009f242fa033109
Ranga Rathnayake <58025781+RangaRathnayake@users.noreply.github.com>
2022-03-13 14:44:17 +0530

    updatge

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,29 +1,28 @@
+/* eslint-disable prettier/prettier */
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 
 @Controller('customer')
 export class CustomerController {
-    constructor(
-        private customerService: CustomerService
-    ) { }
+  constructor(private customerService: CustomerService) {}
 
-    @Post('save')
-    async save(@Body('customer') customer) {
-        return await this.customerService.save(customer);
-    }
+  @Post('save')
+  async save(@Body('customer') customer) {
+    return await this.customerService.save(customer);
+  }
 
-    @Get('all')
-    async getAll() {
-        return await this.customerService.getAll();
-    }
+  @Get('all')
+  async getAll() {
+    return await this.customerService.getAll();
+  }
 
-    @Get(':id')
-    async getById(@Param('id') id) {
-        return await this.customerService.getById(id);
-    }
+  @Get(':id')
+  async getById(@Param('id') id) {
+    return await this.customerService.getById(id);
+  }
 
-    @Get('mobile/:mobile')
-    async getByMobile(@Param('mobile') mobile) {
-        return await this.customerService.getByMobile(mobile);
-    }
+  @Get('mobile/:mobile')
+  async getByMobile(@Param('mobile') mobile) {
+    return await this.customerService.getByMobile(mobile);
+  }
 }
