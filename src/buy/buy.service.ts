d6bf38108843f221585c36ed332e31657c93f182
Ranga Rathnayake <58025781+RangaRathnayake@users.noreply.github.com>
2023-05-22 10:20:13 +0530

    save buy items to table

--- src/buy/buy.service.ts
+++ src/buy/buy.service.ts
@@ -3,15 +3,26 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Buy } from './buy.entity';
+import { BuyItem } from './buy_item.entity';
 
 @Injectable()
 export class BuyService {
   constructor(
     @InjectRepository(Buy) private readonly buyRepository: Repository<Buy>,
-  ) {}
+    @InjectRepository(BuyItem) private readonly buyItemRepo: Repository<BuyItem>
+  ) { }
 
   async save(buy): Promise<Buy> {
-    return await this.buyRepository.save(buy);
+    try {
+      const b = await this.buyRepository.save(buy);
+      await buy.buyItems.forEach(async element => {
+        await this.buyItemRepo.save(element);
+      });
+      return b;
+    } catch (error) {
+      console.log(error);
+    }
+
   }
 
   async getAll() {
